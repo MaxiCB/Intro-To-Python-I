0b0d594947b8c404fa56e956ae1ed48ef32f1701
Aaron Beasley <aaroncb@aaroncb.com>
2020-02-10 16:11:47 -0600

    ADD Distance Exercise to Tuples
    - Added and completed distance calculation based on two tuple inputs

--- src/06_tuples.py
+++ src/06_tuples.py
@@ -1,5 +1,19 @@
 #  Write a function 'print_tuple' that prints all values in a tuple
 
+import math
+
+def dist(a, b):
+    # This destructures the parts of the tuples provided
+    x0, y0 = a 
+    x1, y1 = b
+
+    return math.sqrt((x1-x0)**2 + (y1-y0)**2)
+
+a = (2,7)
+b = (-14, 72)
+
+print("Distance is: {:.2f}".format(dist(a, b)))
+
 def print_tuple(tuple):
     for item in tuple:
         print(item)
