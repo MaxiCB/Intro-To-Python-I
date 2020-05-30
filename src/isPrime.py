6cb4915bb9ec007bee8101a4132f5af56ed7843b
Aaron Beasley <aaroncb@aaroncb.com>
2020-05-30 01:38:14 -0500

    ADD Comments && Improved isPrime
    - Added comments to the existing isPrime functionality
    - Further iterated on isPrime function

--- src/isPrime.py
+++ src/isPrime.py
@@ -1,6 +1,7 @@
 #  Given a number provided by the command line determine if it is a prime number
 
 import getopt, sys 
+from math import sqrt
   
 argumentList = sys.argv[1:] 
   
@@ -10,16 +11,24 @@ long_options = ["Number"]
 
 number = 0
 
+# A prime number (or a prime) is a natural number greater than 1 that is not a product of two smaller natural numbers.
 def isPrime(p):
-    if p > 1:
-        for i in range(2, p // 2):
-            if (p % i) == 0:
-                return False
-                break
-            else:
-                return True
-    else:
+    # Prime cannot be negative, or 1
+    if(p <= 1):
         return False
+    # 2&&3 are both primes
+    if(p <= 3): 
+        return True
+    # If number is divisible by 2||3 it is not prime
+    if( p % 2 == 0 or p % 3 == 0):
+        return False
+    # Initially set i to next possible prime
+    i = 5
+    while(i * i <= p):
+        if(p % i == 0 or p % (i + 2) == 0):
+            return False
+        i = i + 6
+    return True
     
 try: 
     arguments, values = getopt.getopt(argumentList, options, long_options) 
